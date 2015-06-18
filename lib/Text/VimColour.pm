97a6b11209b781ba0c262d1af652c7ec0adf9c90
Steve Mynott <steve.mynott@gmail.com>
2015-06-18 19:56:46 +0100

    bump to 0.3

--- lib/Text/VimColour.pm
+++ lib/Text/VimColour.pm
@@ -2,7 +2,7 @@
 use v6;
 use File::Temp;
 
-class Text::VimColour:ver<0.2> {
+class Text::VimColour:ver<0.3> {
     subset File of Str where -> $x { so $x && $x.IO.e };
     subset Path of Str where -> $x { so $x && $x.IO.dirname.IO.e } 
     # BUG https://rt.perl.org/Public/Bug/Display.html?id=125245
